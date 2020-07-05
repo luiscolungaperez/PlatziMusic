4251a65900b40c90491a5c79337363ee900d3bc1
luiscolungaperez <perez.luis.colunga@gmail.com>
2020-07-05 17:48:47 -0500

    Preview config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import ViewGeneral from "../views/ViewGeneral";
-import Componente from "./Componente";
+import Register from "../views/Register";
+import Login from "../views/Login";
+import "../assets/styles/App.scss";
 
 const App = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={ViewGeneral} />
-      <Route exact path="/componente" component={Componente} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/login" component={Login} />
     </Switch>
   </BrowserRouter>
 );
